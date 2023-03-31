bae65389cbc5333b6fa3ae3030fc537533f14099
Matthew Auer <matthewauer72@gmail.com>
2023-03-31 13:18:06 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const superbowlWin = (record) => {
+    const foundYear = record.find(element => element.result === 'W');
+    // once W is found, I want to return the year which the W was found
+    // foundYear {year: 2015 and result: 'W'}
+    // foundYear undefined
+    // I want to return the year when there is a W
+    // if foundYear is undefined I want to return undefined
+  if (foundYear === undefined){
+    return undefined
+  }
+
+  else {
+    return foundYear.year
+  }
+}
+
+
+
+
